153f4ebf5cdfa20628f7fe50324a01b2b9b3c77e
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-17 03:19:32 -0300

    adjusting nav to swiper preset

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,7 @@
-<ul id="nav-currency">
-    @foreach ($currencies as $currency)
-        <li class="row col-dt-2 col-mb-6 direction-column center">
-            <h3 class="name">{{$currency->name}}</h3>
-            <span class="value">R$ {{number_format(1/$currency->value, 3, ",", ".")}}</span>
-            <time class="date">{{$currency->updated_at}}</time>
-        </li>
-    @endforeach
-</ul>
+<div id="nav-currency" class="swiper">
+    <div class="swiper-wrapper">
+        @foreach ($currencies as $currency)
+            <x-slide-item :currency="$currency" />
+        @endforeach
+    </div>
+</div>
