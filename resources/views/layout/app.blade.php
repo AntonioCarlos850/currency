84994ba5edf45923f321fee78e56ade63006ecc3
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-17 03:20:03 -0300

    add swiper links

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    {{-- Swiper Lib --}}
+    <link
+        rel="stylesheet"
+        href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
+    />
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
     <link rel="stylesheet" href="{{asset("css/app.css")}}">
     <link rel="stylesheet" href="{{asset("css/navbar.css")}}">
     <title>@stack('title')</title>
@@ -13,6 +21,6 @@
     <x-navbar :currencies="$currencies" />
     @yield('content')
     <x-footer />
-    <script src="{{asset("js/navbar.js")}}"></script>
+    <script src="{{asset('js/swiper.js')}}"></script>
 </body>
 </html>
