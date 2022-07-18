9f741fb740dd855447ec2ef9f0a6e395677aa9aa
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-18 00:28:21 -0300

    change css link name

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -14,12 +14,12 @@
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 
     <link rel="stylesheet" href="{{asset("css/app.css")}}">
-    <link rel="stylesheet" href="{{asset("css/navbar.css")}}">
+    <link rel="stylesheet" href="{{asset("css/topbar.css")}}">
     <title>@stack('title')</title>
     @stack('styles')
 </head>
 <body>
-    <x-navbar :currencies="$currencies" />
+    <x-navbar />
     @yield('content')
     <x-footer />
     <script src="{{asset('js/swiper.js')}}"></script>
