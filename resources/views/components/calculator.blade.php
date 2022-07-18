7aec69abdae528ddba1e35a8badd2226909fd06b
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-17 21:52:28 -0300

    calculator component

---
+++ resources/views/components/calculator.blade.php
@@ -0,0 +1,47 @@
+<div class="row w-100 center my-2" id="calculator">
+    <div class="col-dt-9 col-mb-10 d-flex direction-column center p-1" id="calculator-content">
+        <h2 id="calculator-tittle">Currency Calculator</h2>
+        <time datetime="{{$currencies->first()->updated_at}}">{{$currencies->first()->updated_at}}</time>
+        <h3 id="caption" class="my-1">Convert the currency</h3>
+        <form class="row space-evenly align-baseline py-2">
+            <div class="col-dt-2 col-mb-8">
+                <label class="w-40">Value</label>
+                <input type="number" step="0.01" min="0" id="amount" class="w-100" />
+            </div>
+            <div class="col-dt-3 col-mb-8">
+                <label for="currency-1">Convert from</label>
+                <select class="form-control w-100" id="currency-1" required>
+                    @foreach ($currencies as $currency)
+                        <option>{{$currency->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-dt-3 col-mb-8">
+                <label for="currency-2">Convert to</label>
+                <select class="form-control w-100" id="currency-2" required>
+                    @foreach ($currencies as $currency)
+                        <option>{{$currency->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-dt-1 col-mb-8">
+                <button id="calculate-btn" class="btn btn-primary">Calculate!</button>
+            </div>
+        </form>
+        <div class="row w-100 space-around">
+            <div class="result col-dt-4 col-mb-10">
+                <p><strong>Conversion of: </strong><span class="base-currency"></span></p>
+                <p><strong>Value to convert: </strong><span class="given-amount"></span></p>
+            </div>
+            <div class="result col-dt-4 col-mb-10">
+                <p><strong>To: </strong><span class="second-currency"></span></p>
+                <p><strong>Conversion result: </strong><span class="final-result"></span></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    var crrncy = {!! collect($javascript)->toJson() !!}
+</script>
+<script src="{{asset('js/calculator.js')}}"></script>
