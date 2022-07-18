ce3ff29fae993ca44aef25714d33ee3583e19f32
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-18 00:29:06 -0300

    styling currency page

--- resources/views/currency.blade.php
+++ resources/views/currency.blade.php
@@ -1,2 +1,14 @@
-<h1>{{$currency->name}}</h1>
-<h2>{{$currency->value}}</h2>
+@extends('layout.app')
+
+@push('styles')
+    <link rel="stylesheet" href="{{asset("css/currency.css")}}">
+@endpush
+
+@section('content')
+    <div class="row direction-column p-2" id="content">
+        <div class="col-10 center">
+            <h1>Currency: {{$currency->name}}</h1>
+            <h2>R$ 1,00 = {{$currency->value}}</h2>
+        </article>
+    </div>
+@endsection
