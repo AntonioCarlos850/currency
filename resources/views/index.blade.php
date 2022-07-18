7f4232709c50500720d179691cb26196c0cca5a6
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-17 21:53:01 -0300

    add more css to home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,11 @@
     Home
 @endpush
 
+@push('styles')
+    <link rel="stylesheet" href="{{asset("css/calculator.css")}}">
+    <link rel="stylesheet" href="{{asset("css/index.css")}}">
+@endpush
+
 @section('content')
-    
+    <x-calculator :javascript="$currency_javascript_format" :currencies="$currencies" />
 @endsection
