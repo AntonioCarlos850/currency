59d412db41e0f2b4bc0f5313e28dba9af0ad2375
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-18 00:26:01 -0300

    add link to nav item

--- resources/views/components/slide-item.blade.php
+++ resources/views/components/slide-item.blade.php
@@ -1,5 +1,7 @@
 <div class="swiper-slide">
-    <h3 class="name">{{$currency->name}}</h3>
-    <span class="value">R$ {{$currency->value}}</span>
-    <time class="date">{{$currency->updated_at}}</time>
+    <a href="{{route('currency', $currency->name)}}" class="row center direction-column">
+        <h3 class="name">{{$currency->name}}</h3>
+        <span class="value">R$ {{$currency->value}}</span>
+        <time class="date">{{$currency->updated_at}}</time>
+    </a>
 </div>
