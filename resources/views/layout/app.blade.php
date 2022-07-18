26fa691e96ad1ebb72cebe446b70ea5f7fba4892
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-07-17 21:52:41 -0300

    add style stack on layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="{{asset("css/app.css")}}">
     <link rel="stylesheet" href="{{asset("css/navbar.css")}}">
     <title>@stack('title')</title>
+    @stack('styles')
 </head>
 <body>
     <x-navbar :currencies="$currencies" />
